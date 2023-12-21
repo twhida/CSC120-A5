a61464e3622831b189ebb046fdebfa8e32f45125
Tomoko Hida <thida@smith.edu>
2023-12-21 17:41:30 -0500

    engineUpdate

--- Engine.java
+++ Engine.java
@@ -3,20 +3,32 @@ public class Engine {
     double currentFuel;
     double maxFuelLevel;
 
-public Engine(double currentFuel, double maxFuelLevel){
-    this.currentFuel = currentFuel;
-    this.maxFuelLevel = maxFuelLevel;
-}
-
-public void refuel(){
-    currentFuel = maxFuelLevel;
-}
-
-public void go(){
-    while (currentFuel > 0){
-        currentFuel -= 1;
+    public Engine(FuelType f, double currentFuel, double maxFuelLevel){
+        this.f = FuelType;
+        this.currentFuel = currentFuel;
+        this.maxFuelLevel = maxFuelLevel;
     }
-    refuel();
-}
 
+    public void refuel(){
+        currentFuel = maxFuelLevel;
     }
+
+    public void go(){
+        while (currentFuel > 0){
+            currentFuel -= 1;
+        }
+        refuel();
+    }
+
+    public static void main(String[] args) {
+        Engine myEngine = new Engine(FuelType.ELECTRIC, 100.0);
+        try {
+            while (true) {
+                myEngine.go();
+            }
+        } catch (Exception e) {
+            System.err.println(e.getMessage()); // Out of fuel
+        }
+    }
+
+}

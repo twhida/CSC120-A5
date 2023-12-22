7ae0b4df5f1d2f6f10ab0aac494efdad1f07e52c
Tomoko Hida <thida@smith.edu>
2023-12-21 21:40:10 -0500

    changes

--- Passenger.java
+++ Passenger.java
@@ -1,4 +1,3 @@
-import java.lang.RuntimeException;
 
 public class Passenger {
     private String name;
@@ -6,17 +5,24 @@ public class Passenger {
     public Passenger(String name) {
         this.name = name;
     }
+
     public void boardCar(Car c){
     try {
          c.addPassenger(this);
     } catch (Exception e) {
-
-     System.out.println("This car is full!");
+        System.out.println("This car is full!");
     }
+    }
+
     public void getOffCar(Car c){
+    try { 
         c.removePassenger(this);
+    } catch (Exception e) {
+        System.out.println("This passenger is not in this car.");
     }
     
+
+    }
 }
 
-}
+
--- Train.java
+++ Train.java
@@ -1,3 +1,43 @@
-public class Train {
+import java.util.*;
 
+public class Train {
+    private final Engine engine ;
+    private ArrayList<Car> carsCurrentlyAttached;
+    int nCars;
+    int passengerCapacity;
+    FuelType f;
+    double fuelCapacity;
+
+    public Train(FuelType f, double fuelCapacity, int nCars, int passengerCapacity){
+        this.f = f;
+        this.fuelCapacity = 30;
+        this.nCars = nCars;
+        this.passengerCapacity = passengerCapacity;
+    }
+    
+    public Engine getEngine(){
+        return this.engine;
+    }
+
+    public Car getCar(int i){
+        return this.getCar(i);
+    }
+
+    public int getMaxCapacity(){
+        return this.getMaxCapacity();
+    }
+
+    public int seatsRemaining(){
+        return this.seatsRemaining();
+    }
+
+    public void printManifest(){
+        if(carsCurrentlyAttached.size() == 0){
+            System.out.println("No cars are attached to this train.");
+        }
+        else System.out.println("Passengers on board:" + this.carsCurrentlyAttached);
+    }
+
+
+    
 }
